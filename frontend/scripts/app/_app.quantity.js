c4d4fde6eec879ce2db1d18085206a886904b357
ed <ed.proff@gmail.com>
2016-10-27 03:42:41 +0300

    update quantity

--- frontend/scripts/app/_app.quantity.js
+++ frontend/scripts/app/_app.quantity.js
@@ -7,10 +7,12 @@ var app = app || {};
 
         config: {
             element: '.j-quantity',
-            count: '.j-quantity-count',
+            input: '.j-quantity-count',
             control: '.j-quantity-control',
             complete: null
         },
+
+        element: null,
 
         extend: function(config)
         {
@@ -27,53 +29,96 @@ var app = app || {};
             }
         },
 
+        setValue: function(quantity)
+        {
+            var min = 1, max = 100;
+
+            if (this.element.data('min')) {
+                min = this.element.data('min');   
+            }
+            
+            if (this.element.data('max')) {
+                max = this.element.data('max');   
+            }
+
+            if (quantity > max) {
+                quantity = max;
+            }
+            
+            if (quantity < min) {
+                quantity = min;
+            }
+
+            this.element.find(this.config.input).val(quantity);
+
+        },
+
+        increase: function(quantity)
+        {
+            quantity += 1;
+
+            this.setValue(quantity);
+        },
+
+        decrease: function(quantity)
+        {
+            if (quantity > 1) {
+                quantity -= 1;
+            }
+
+            this.setValue(quantity);
+        },
+
+        callback: function()
+        {
+            if (typeof(this.element.data('product')) !== 'undefined' && typeof(this.config.complete) == 'function')
+            {
+                this.config.complete.call(null, this.element, this.element.data('product'));
+            }
+        },
+
+        keys: function()
+        {
+            var _this = this, role = '';
+
+            $('body').on('keydown', _this.config.input, function(e) {
+                if ([38, 40].indexOf(e.keyCode) >= 0)
+                {
+                    e.preventDefault();
+
+                    role = {
+                        38: 'increase',
+                        40: 'decrease'
+                    };
+
+                    _this.element = $(this).closest(_this.config.element);
+
+                    _this[role[e.keyCode]](parseInt(_this.element.find(_this.config.input).val()));
+
+                    _this.callback();
+
+                    return false;
+                }
+            });
+        },
+
         bind: function()
         {
-            var _this = this, product = '', quantity = 0, min = 1, max = 100, $element = '', method = '';
+            var _this = this, role = '';
 
             $('body').on('click', _this.config.control, function(e) {
                 e.preventDefault();
 
-                $element = $(this).closest(_this.config.element);
+                _this.element = $(this).closest(_this.config.element);
 
-                method = $(this).data('method');
-                product = $element.data('product');
-                quantity = parseInt($element.find(_this.config.count).val());
-
-                if ($element.data('min')) {
-                    min = $element.data('min');   
-                }
-                
-                if ($element.data('max')) {
-                    max = $element.data('max');   
-                }
-
-                switch(method) {
-                    case 'increase':
-                        quantity += 1;
-                    break;
-
-                    case 'decrease':
-                        if (quantity > 1) {
-                            quantity -= 1;
-                        }
-                    break;
-                }
-
-                if (quantity > max) {
-                    quantity = max;
-                }
-                
-                if (quantity < min) {
-                    quantity = min;
-                }
-                
-                $element.find(_this.config.count).val(quantity);
-
-                if (typeof(_this.config.complete) == 'function')
+                role = $(this).data('role');
+         
+                if(['increase', 'decrease'].indexOf(role) >= 0)
                 {
-                    _this.config.complete.call(null, $element, product);
+                    _this[role](parseInt(_this.element.find(_this.config.input).val()));
                 }
+
+                _this.callback();
 
                 return !1;
             });
@@ -81,11 +126,13 @@ var app = app || {};
 
         init: function(config)
         {
-            if (typeof config !== 'undefined') {
+            if (typeof config !== 'undefined')
+            {
                 this.extend(config);
             }
 
             this.bind();
+            this.keys();
         }
 
     };
@@ -103,7 +150,7 @@ var app = app || {};
 // });
 
 // <div class="quantity j-quantity clearfix" data-product="{$product.id}" data-min="1" data-max="999">
-//     <button type="button" class="quantity__control _decrease j-quantity-control j-quantity-recount" data-method="decrease"></button>
-//     <input name="count[{$product.id}]" class="quantity__count j-quantity-recount j-quantity-count" value="{$product.cart_count}" maxlength="4">
-//     <button type="button" class="quantity__control _increase j-quantity-control j-quantity-recount" data-method="increase"></button>
+//     <button type="button" class="quantity__control _decrease j-quantity-control" data-role="decrease"></button>
+//     <input type="text" name="count[{$product.id}]" value="{$product.cart_count}" data-role="quantity-input" class="quantity__count j-quantity-count" maxlength="3" autocomplete="off">
+//     <button type="button" class="quantity__control _increase j-quantity-control" data-role="increase"></button>
 // </div>
