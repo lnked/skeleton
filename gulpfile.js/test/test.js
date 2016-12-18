a54c76cb95a7dd777230c6b9c30e391b0ba86aac
ed <ed.proff@gmail.com>
2016-12-18 21:46:44 +0300

    update

---
+++ gulpfile.js/test/test.js
@@ -0,0 +1,130 @@
+describe('hooks', function() {
+
+  before(function() {
+    // runs before all tests in this block
+  });
+
+  after(function() {
+    // runs after all tests in this block
+  });
+
+  beforeEach(function() {
+    // runs before each test in this block
+  });
+
+  afterEach(function() {
+    // runs after each test in this block
+  });
+
+  // test cases
+});
+
+beforeEach(function() {
+  // beforeEach hook
+});
+
+beforeEach(function namedFun() {
+  // beforeEach:namedFun
+});
+
+beforeEach('some description', function() {
+  // beforeEach:some description
+});
+
+
+describe('Connection', function() {
+  var db = new Connection,
+    tobi = new User('tobi'),
+    loki = new User('loki'),
+    jane = new User('jane');
+
+  beforeEach(function(done) {
+    db.clear(function(err) {
+      if (err) return done(err);
+      db.save([tobi, loki, jane], done);
+    });
+  });
+
+  describe('#find()', function() {
+    it('respond with matching records', function(done) {
+      db.find({type: 'User'}, function(err, res) {
+        if (err) return done(err);
+        res.should.have.length(3);
+        done();
+      });
+    });
+  });
+});
+
+
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
+
+describe('User', function() {
+  describe('#save()', function() {
+    it('should save without error', function(done) {
+      var user = new User('Luna');
+      user.save(function(err) {
+        if (err) done(err);
+        else done();
+      });
+    });
+  });
+});
+
+
+describe('User', function() {
+  describe('#save()', function() {
+    it('should save without error', function(done) {
+      var user = new User('Luna');
+      user.save(done);
+    });
+  });
+});
+
+
+beforeEach(function() {
+  return db.clear()
+    .then(function() {
+      return db.save([tobi, loki, jane]);
+    });
+});
+
+describe('#find()', function() {
+  it('respond with matching records', function() {
+    return db.find({ type: 'User' }).should.eventually.have.length(3);
+  });
+});
+
+
+
+describe('Array', function() {
+  describe.only('#indexOf()', function() {
+    it('should return -1 unless present', function() {
+      // this test will be run
+    });
+
+    it('should return the index when present', function() {
+      // this test will also be run
+    });
+  });
+
+  describe.only('#concat()', function () {
+    it('should return a new Array', function () {
+      // this test will also be run
+    });
+  });
+
+  describe('#slice()', function () {
+    it('should return a new Array', function () {
+      // this test will not be run
+    });
+  });
+});
+But
