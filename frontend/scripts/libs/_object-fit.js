58282930ef21960596acd9fa442e4adc53606e2b
ed <ed.proff@gmail.com>
2017-06-08 11:40:28 +0300

    updage

---
+++ frontend/scripts/libs/_object-fit.js
@@ -0,0 +1,5 @@
+objectFit.polyfill({
+    selector: 'img', // this can be any CSS selector
+    fittype: 'cover', // either contain, cover, fill or none
+    disableCrossDomain: 'true' // either 'true' or 'false' to not parse external CSS files.
+});
