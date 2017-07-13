271eb04e57b1ace30ff70c1b99e81a9bd1422228
ed <ed.proff@gmail.com>
2017-07-14 00:56:20 +0300

    update

---
+++ frontend/scripts/libs/memoize.js
@@ -0,0 +1,58 @@
+// простая чистая функция, которая возвращает сумму аргумента и 10
+const add = (n) => (n + 10);
+console.log('Simple call', add(3));
+// простая функция, принимающая другую функцию и
+// возвращающая её же, но с мемоизацией
+const memoize = (fn) => {
+  let cache = {};
+  return (...args) => {
+    let n = args[0];  // тут работаем с единственным аргументом
+    if (n in cache) {
+      console.log('Fetching from cache');
+      return cache[n];
+    }
+    else {
+      console.log('Calculating result');
+      let result = fn(n);
+      cache[n] = result;
+      return result;
+    }
+  }
+}
+// создание функции с мемоизацией из чистой функции 'add'
+const memoizedAdd = memoize(add);
+console.log(memoizedAdd(3));  // вычислено
+console.log(memoizedAdd(3));  // взято из кэша
+console.log(memoizedAdd(4));  // вычислено
+console.log(memoizedAdd(4));  // взято из кэша
+
+
+// уже знакомая нам функция memoize
+const memoize = (fn) => {
+  let cache = {};
+  return (...args) => {
+    let n = args[0];
+    if (n in cache) {
+      console.log('Fetching from cache', n);
+      return cache[n];
+    }
+    else {
+      console.log('Calculating result', n);
+      let result = fn(n);
+      cache[n] = result;
+      return result;
+    }
+  }
+}
+const factorial = memoize(
+  (x) => {
+    if (x === 0) {
+      return 1;
+    }
+    else {
+      return x * factorial(x - 1);
+    }
+  }
+);
+console.log(factorial(5)); // вычислено
+console.log(factorial(6)); // вычислено для 6, но для предыдущих значений взято из кэша
---
+++ frontend/scripts/tests/_left-pad.js
@@ -0,0 +1,14 @@
+const leftPad = require('left-pad');
+// import leftPad from 'left-pad';
+
+leftPad('foo', 5)
+// => "  foo"
+
+leftPad('foobar', 6)
+// => "foobar"
+
+leftPad(1, 2, '0')
+// => "01"
+
+leftPad(17, 5, 0)
+// => "00017"
