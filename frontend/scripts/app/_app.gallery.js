0d0e3dc5d78ccb008d197de2c47e1077fa4016fe
ed <ed.proff@gmail.com>
2017-10-24 19:51:40 +0300

    update

--- frontend/scripts/app/_app.gallery.js
+++ frontend/scripts/app/_app.gallery.js
@@ -4,9 +4,39 @@ let app = app || {};
     "use strict";
 
     app.gallery = {
-        init: function()
-        {
-            $('.zoom').fancybox();
+
+        bind(selector = '.zoom') {
+            if (document.querySelectorAll(selector).length) {
+                $(selector).fancybox({
+                    loop: true,
+                    protect: true,
+                    toolbar  : true,
+                    smallBtn : true,
+                    buttons : [
+                        'download',
+                        'slideShow',
+                        'close'
+                    ],
+                    margin : [50, 50],
+                    image : {
+                        preload : "auto",
+                    },
+                    animationEffect : "fade",
+                    transitionEffect : "fade",
+                    clickContent : function( current, event ) {
+                        return current.type === 'image' ? 'next' : 'close';
+                    },
+                    mobile : {
+                        dblclickContent : function( current, event ) {
+                            return current.type === 'image' ? 'zoom' : false;
+                        }
+                    }
+                });
+            }
+        },
+
+        init() {
+            this.bind('.zoom');
         }
     };
 
