e525d51ddc98eed898f224860ea631ba77f966fc
ed <ed.proff@gmail.com>
2017-11-07 19:22:07 +0300

    add modal

---
+++ frontend/scripts/libs/_add-script.js
@@ -0,0 +1,7 @@
+// (function(d) {
+//     var e = d.createElement(s),
+//         s = d.getElementsByTagName('script')[0];
+//     e.src = 'https://.../path/to/script.js';
+//     e.async = true;
+//     s.parentNode.insertBefore(e, s);
+// })(document);
