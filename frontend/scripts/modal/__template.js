ac954c11a5cb469616341fe3ddccf1b52a344f39
ed <ed.proff@gmail.com>
2017-12-27 19:08:11 +0300

    upgrade dependencies

---
+++ frontend/scripts/modal/__template.js
@@ -0,0 +1,23 @@
+(d => {
+    window.template = (id, data, precompile) => {
+        if (typeof precompile === "undefined") {
+            precompile = false;
+        }
+
+        if (d.getElementById(id) !== null) {
+            const pattern = d.getElementById(id).innerHTML;
+
+            if (precompile) {
+                if (!window.precompiledT7) {
+                    window.precompiledT7 = Template7.compile(pattern);
+                }
+
+                return window.precompiledT7(data || {});
+            }
+
+            return Template7.compile(pattern)(data || {});
+        }
+
+        return "";
+    };
+})(document);
